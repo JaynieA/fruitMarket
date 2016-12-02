b791b30545d4a8db4f363bbfc5cdaf6f90c1aff6
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2016-12-02 07:51:21 -0600

    changed button id declarations to class name's

--- scripts/script.js
+++ scripts/script.js
@@ -57,8 +57,8 @@ function displayStoreFruit(fruitArray, displayId) {
     DOM */
     var htmlString = '';
     fruitArray.forEach(function(fruit) {
-        htmlString += '<div class="col-sm-3"><div class="marketFruit"><button type="button" class="buyFruit"';
-        htmlString += 'name="' + fruit.type + '" id="' + fruit.type + '">';
+        htmlString += '<div class="col-sm-3"><div class="marketFruit"><button type="button" class="buyFruit ' + fruit.type + '"';
+        htmlString += 'name="' + fruit.type + '">';
         htmlString += '<p>' + fruit.price.toLocaleString('en-US', {
             style: 'currency',
             currency: 'USD'
@@ -218,7 +218,7 @@ var displayUserStock = function() {
     var outputText = '';
     sortArray(userStock);
     for (var i = 0; i < userStock.length; i++) {
-        outputText += '<button class="sellFruit" id="' + userStock[i].type + '"></button>';
+        outputText += '<button class="sellFruit ' + userStock[i].type + '"></button>';
     }
     //display results of userStock to the DOM
     $('#userStock').html(outputText);
